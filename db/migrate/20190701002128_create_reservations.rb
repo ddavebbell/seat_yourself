56c54f8c22b1cbd270bbbec765335340a5b07ca4
Dave <daverichardbell@gmail.com>
2019-07-02 19:25:20 -0400

    Update reservation migration

--- db/migrate/20190701002128_create_reservations.rb
+++ db/migrate/20190701002128_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[5.2]
   def change
     create_table :reservations do |t|
-      t.datetime :reservation_date
-      t.integer :customer_loyalty_points
+      t.datetime :reservation_time
+      t.integer :customer_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2019_07_01_002128) do
 
   create_table "reservations", force: :cascade do |t|
-    t.datetime "reservation_date"
-    t.integer "customer_loyalty_points"
+    t.datetime "reservation_time"
+    t.integer "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
