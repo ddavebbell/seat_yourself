ac3a91b084e7100283ea9dbaf3c03da781778eef
Dave <daverichardbell@gmail.com>
2019-07-02 19:28:50 -0400

    Create and run OpenDay migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_002128) do
+ActiveRecord::Schema.define(version: 2019_07_02_232715) do
+
+  create_table "open_days", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "reservation_time"
