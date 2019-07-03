cc1c55ec4d3937338c5ceab6b14d67c226deb3be
ddavebbell <50187597+ddavebbell@users.noreply.github.com>
2019-07-03 15:16:30 -0400

    Restaurant owner permission edit (#10)
    * restaurant and reservation migration alteration added date and time
    
    * Profile page is working except update opentimes form doesnt work
    
    * got dummy version of restaurant owner can edit open and close times. need to fix day aspect to time stored in open_time, closed_time
    
    * validation error in Restaurant Edit route http://localhost:3000/restaurants/1/edit
    
    * made restaurant edit and show links and nav bar more clear for understanding
    
    * made restaurant index page Restaurant names clickable to goto show page
    
    * changed the things according to review

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,7 +5,19 @@ class RestaurantsController < ApplicationController
 
 	def show
 		@restaurant = Restaurant.find(params[:id])
-
 	end
 
+	def update
+		@restaurant = Restaurant.find(params[:id])
+
+		@restaurant.assign_attributes({
+		open_time:	params[:restaurant][:open_time],
+		closing_time: params[:restaurant][:closing_time]	}).save
+
+		redirect_to restaurant_path
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
 end
