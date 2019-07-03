75186c9f1a8acfb5c2314bf86d695d244cc0aff6
ddavebbell <50187597+ddavebbell@users.noreply.github.com>
2019-07-02 21:16:52 -0400

    started login form for user (#6)
    * staretd login form for user
    
    * finished index page for now, empty links
    
    * changed code according to review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,11 @@
-class UsersController < ApplicationController; end
+class UsersController < ApplicationController
+
+	def index
+		@user = User.new
+	end
+
+	def edit
+		@user = User.find(params[:email],params[:password_digest])
+	end
+
+end
