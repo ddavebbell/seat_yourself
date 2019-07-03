8dce9f1d69433d156f26eb0fd134a360a26b1623
somethinrother <jamesneilson4@gmail.com>
2019-07-02 22:35:39 -0400

    Improve current_user method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,6 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      User.find_by(id: session[:user_id])
-    end
+    @current_user = @current_user || User.find_by(id: session[:user_id])
   end
 end
